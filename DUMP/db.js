ff746850129aaaaa326e02133c3cdc90e5fa8337
echao <echao8@uwo.ca>
2023-12-04 17:26:11 -0500

    Connection works

--- DUMP/db.js
+++ DUMP/db.js
@@ -1,14 +1,19 @@
 const { createConnection } = require('mysql');
 
+
+// HOW TO USE:
+// do 'npm i mysql' to download dependencies
+// also do 'npm -y' to initialize package.json
 const connection = createConnection({
     host: 'localhost',
     user: 'root',
+    // CHANGE WITH YOUR MYSQL PASSWORD
     password: '123!@#QWEasdzxc',
     database: 'Potify',
     port: 3306,
 });
 
-connection.query('SELECT * FROM songs', (err, rows) => {
+connection.query('SELECT * FROM Song', (err, rows) => {
     if (err) throw err;
 
     console.log('Data received from Db:');
